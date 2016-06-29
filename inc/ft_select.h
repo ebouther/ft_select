89c58f563f7ce9e9e1dc73d6d0874f7caff0cec0
ebouther <ebouther@student.42.fr>
2016-06-29 12:06:14 +0200

    ESC_KEY

--- inc/ft_select.h
+++ inc/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: ebouther <ebouther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/28 21:02:12 by ebouther          #+#    #+#             */
-/*   Updated: 2016/06/09 23:09:12 by ebouther         ###   ########.fr       */
+/*   Updated: 2016/06/21 16:24:01 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,21 @@
 
 # define LEFT_KEY 68
 # define RIGHT_KEY 67
-# define UP_KEY 172052251
-# define DOWN_KEY 172117787
-# define ESC_KEY 2587
-# define SPACE_KEY 32
-# define DEL_KEY 127
+# define ESC_KEY 87
 # define RET_KEY 10
-# define SUP_KEY 2117294875
-# define HOME_KEY 4741915
-# define END_KEY 4610843
-# define PAGE_UP_KEY 2117425947
-# define PAGE_DOWN_KEY 2117491483
-# define TAB_KEY 9
-# define CRTL_A_KEY 1
 
-typedef struct	s_termcap
+typedef struct		s_termcap
 {
-	char	*termtype;
-	char	PC;
-	char	*BC;
-	char	*UP;
-	char	*cl_string;
-	char	*cm_string;
-	int		height;
-	int		width;
-	int		auto_wrap;
-}				t_termcap;
+	char			*termtype;
+	char			PC;
+	char			*BC;
+	char			*UP;
+	char			*cl_string;
+	char			*cm_string;
+	int				height;
+	int				width;
+	int				auto_wrap;
+}					t_termcap;
 
 enum				e_mode
 {
